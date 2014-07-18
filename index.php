2a7ffd56038350b5791f53e9dbb1953b4f230e78
John A. Huebner II <Hube2@users.noreply.github.com>
2014-07-18 15:31:59 -0400

    Corrected Whitespace

--- index.php
+++ index.php
@@ -22,41 +22,41 @@
 	if (!BLUNT_CACHE_FULL_PAGE || !apply_filters('blunt_cache_frag_check', false, $key)) {
 		get_header();
 		?>
-      <div id="content" itemscope itemtype="http://schema.org/Blog">
-        <h1 id="page-title">Blog</h1>
-        <?php 
-          if (have_posts()) {
-            blunt_archive_nav('before');
-            ?>
-              <div class="articles">
-                <?php 
-                  while (have_posts()) {
-                    the_post();
-                    ?>
-                      <div class="article" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
-                       <h2 itemprop="name"><a itemprop="url" href="<?php 
-                            the_permalink(); ?>"><span itemprop="name"><?php the_title(); ?></span></a></h2>
-                        <?php blunt_post_meta(); ?>
-                        <p><?php the_excerpt(); ?></p>
-                        <p><a href="<?php the_permalink(); ?>">Continue Reading</a></p>
-                      </div>
-                    <?php 
-                  } // end while have posts
-                ?>
-              </div>
-            <?php 
-            blunt_archive_nav('after');
-          } else {
-            // no posts
-            ?>
-              <p>No results were found.</p>
-            <?php 
-          }
-        ?>
-      </div>
-      <div id="sidebar-after-content">
-        <?php get_sidebar('blog'); ?>
-      </div>
+			<div id="content" itemscope itemtype="http://schema.org/Blog">
+				<h1 id="page-title">Blog</h1>
+				<?php 
+					if (have_posts()) {
+						blunt_archive_nav('before');
+						?>
+							<div class="articles">
+								<?php 
+									while (have_posts()) {
+										the_post();
+										?>
+											<div class="article" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+											 <h2 itemprop="name"><a itemprop="url" href="<?php 
+														the_permalink(); ?>"><span itemprop="name"><?php the_title(); ?></span></a></h2>
+												<?php blunt_post_meta(); ?>
+												<p><?php the_excerpt(); ?></p>
+												<p><a href="<?php the_permalink(); ?>">Continue Reading</a></p>
+											</div>
+										<?php 
+									} // end while have posts
+								?>
+							</div>
+						<?php 
+						blunt_archive_nav('after');
+					} else {
+						// no posts
+						?>
+							<p>No results were found.</p>
+						<?php 
+					}
+				?>
+			</div>
+			<div id="sidebar-after-content">
+				<?php get_sidebar('blog'); ?>
+			</div>
 		<?php 
 		get_footer();
 	}
--- sidebar.php
+++ sidebar.php
@@ -4,5 +4,5 @@
 	*/
 ?>
 <div class="widget-area">
-  <?php dynamic_sidebar('blog-sidebar'); ?>
+	<?php dynamic_sidebar('blog-sidebar'); ?>
 </div>
