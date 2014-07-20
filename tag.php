890c8efeda586170cf6b033795c0ef0f87b106ce
John A. Huebner II <Hube2@users.noreply.github.com>
2014-07-20 11:33:24 -0400

    added translation function to heading

--- tag.php
+++ tag.php
@@ -8,7 +8,7 @@
 		get_header();
 		?>
 			<div id="content" itemscope itemtype="http://schema.org/Blog">
-				<h1 id="page-title">Tagged: <?php single_tag_title(); ?></h1>
+				<h1 id="page-title"><?php echo __('Tagged'),': ',single_tag_title(); ?></h1>
 				<?php 
 					if (have_posts()) {
 						blunt_archive_nav('before');
